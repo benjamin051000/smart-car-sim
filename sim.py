7a1c13c7639e067755044e562224ec523308208f
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2022-04-06 23:27:39 -0400

    all draw functionality within one function. cars now use x and y coords on a "grid"

--- sim.py
+++ sim.py
@@ -26,75 +26,88 @@ class Car:
     CAR_WIDTH = 100
     CAR_HEIGHT = 50
 
-    CAR_X_SPEED = CAR_WIDTH
     CAR_X_START = 0
 
-    lane_heights = {
-        1: HEIGHT // 2 - CAR_HEIGHT // 2 + 5 - 200,
-        2: HEIGHT // 2 - CAR_HEIGHT // 2 + 5 - 100,
-        3: HEIGHT // 2 - CAR_HEIGHT // 2 + 5,
-        4: HEIGHT // 2 - CAR_HEIGHT // 2 + 5 + 100,
-        5: HEIGHT // 2 - CAR_HEIGHT // 2 + 5 + 200,
-    }
-
-    def __init__(self, current_lane: int, goal_lane: int):
-        # rect for drawing
-        self.rect = Rect(Car.CAR_X_START, Car.lane_heights[current_lane], Car.CAR_WIDTH, Car.CAR_HEIGHT)
-        self.color = Color('white')
-
+    def __init__(self, current_lane: int, goal_lane: int, color: Color, network: CarNetwork):
+        # Current lane (y-position on the coordinate grid)
         self.current_lane = current_lane
-        self.goal_lane = goal_lane
-        
-        self.intent: Car.Intent = Car.Intent.NO_CHANGE  # TODO broadcast via a @property
 
-        self.intent_messages = []
-    
+        # Lane this Car wants to be in in the future
+        self.goal_lane = goal_lane
+
+        # X-position on the coordinate grid
+        self.x_pos = self.CAR_X_START
+        print(f"X POS: {self.x_pos}")
+
+        self.color = color
+        self.network = network
+
     def change_lanes(self, new_lane: int):
         """Update grahpics of car to new lane, also store new lane in self.current_lane"""
-        self.rect.top = Car.lane_heights[new_lane]
+        # self.rect.top = Car.lane_heights[new_lane]
         self.current_lane = new_lane
 
     def drive(self):
         """Move car forward CAR_X_SPEED pixels."""
-        self.rect.move_ip(Car.CAR_X_SPEED, 0)  # drive forward
+        self.x_pos += 1  # drive forward one space on the grid
 
         # Choose whether or not to change lanes
         if self.current_lane != self.goal_lane:
             # Express intent here
-            if self.current_lane < self.goal_lane:
-                self.intent = Car.Intent.LANE_CHANGE_UP
-            elif self.current_lane > self.goal_lane:
-                self.intent = Car.Intent.LANE_CHANGE_DOWN
-            else:
-                self.intent = Car.Intent.NO_CHANGE
+            # if self.current_lane < self.goal_lane:
+            #     self.intent = Car.Intent.LANE_CHANGE_UP
+            # elif self.current_lane > self.goal_lane:
+            #     self.intent = Car.Intent.LANE_CHANGE_DOWN
+            # else:
+            #     self.intent = Car.Intent.NO_CHANGE
             # Figure out if safe/smart to change lane (smart part of all this)
 
-            #perform algorithms here
+            # perform algorithms here
 
             # Actually change lanes
-            if(self.current_lane < self.goal_lane):
+            if self.current_lane < self.goal_lane:
                 # TODO express intent to other cars!
                 self.change_lanes(self.current_lane + 1)
             else:
                 self.change_lanes(self.current_lane - 1)
-    
-    def draw(self, surf):
-        pygame.draw.rect(surf, self.color, self.rect)
+
+    def draw(self, surf: pygame.surface.Surface):
+        """Convert coordinate grid points to pixel values
+        and draw the rectangle on the surface."""
+        X_GRIDSIZE = self.CAR_WIDTH
+
+        x_pix = X_GRIDSIZE * self.x_pos
+        lane_heights = {  # TODO make function
+            1: HEIGHT // 2 - self.CAR_HEIGHT // 2 + 5 - 200,
+            2: HEIGHT // 2 - self.CAR_HEIGHT // 2 + 5 - 100,
+            3: HEIGHT // 2 - self.CAR_HEIGHT // 2 + 5,
+            4: HEIGHT // 2 - self.CAR_HEIGHT // 2 + 5 + 100,
+            5: HEIGHT // 2 - self.CAR_HEIGHT // 2 + 5 + 200,
+        }
+        y_pix = lane_heights[self.current_lane]
+        # Create rect
+        rect = Rect(x_pix, y_pix, self.CAR_WIDTH, self.CAR_HEIGHT)
+        # Draw that bih
+        pygame.draw.rect(surf, self.color, rect)
+
 
 # Define objects on the screen
 
 
 # On-screen objects (mostly rectangles)
 cars = [
-    Car(1, 4),
-    Car(2, 5),
-    #Car(3, 3),
-    #Car(4, 4),
+    Car(1, 4, Color('green'), network, ),
+    Car(2, 5, Color('blue'), network),
+    # Car(3, 3),
+    # Car(4, 4),
     # Car(5, 1)
 ]
 
 road = Rect(0, 50, WIDTH, HEIGHT - 100)
-lanes = [Rect(0, 2 * i * road.top + road.top, WIDTH, LANE_DIVIDER_WIDTH) for i in range(NUM_LANES + 2)]
+lanes = [
+    Rect(0, 2 * i * road.top + road.top, WIDTH, LANE_DIVIDER_WIDTH)
+    for i in range(NUM_LANES + 2)
+]
 
 
 physics_count = 60
